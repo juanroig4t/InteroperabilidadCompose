491b3dc2a596d4fb2a18f11746e5637526d6c120
juanroig4t <93948627+juanroig4t@users.noreply.github.com>
2024-02-17 13:25:06 +0100

    Xml en compose

--- app/src/main/java/es/juendev/interoperabilidadcompose/MainActivity.kt
+++ app/src/main/java/es/juendev/interoperabilidadcompose/MainActivity.kt
@@ -2,6 +2,7 @@ package es.juendev.interoperabilidadcompose
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.viewinterop.AndroidView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +25,25 @@ class MainActivity : AppCompatActivity() {
                 Text(text = "Compose Fullscreen!!",
                     style= MaterialTheme.typography.headlineLarge,
                     color = MaterialTheme.colorScheme.primary
+                )
+
+                AndroidView(
+                    modifier = Modifier.fillMaxSize(), // Occupy the max size in the Compose UI tree
+                    factory = { context ->
+                        // Creates view
+                        val myView = LayoutInflater.from(context).inflate(R.layout.activity_main, null, false)
+                       // do things
+                        myView
+                    },
+                    update = { view ->
+                        // View's been inflated or state read in this block has been updated
+                        // Add logic here if necessary
+
+                        // As selectedItem is read here, AndroidView will recompose
+                        // whenever the state changes
+                        // Example of Compose -> View communication
+
+                    }
                 )
             }
 
