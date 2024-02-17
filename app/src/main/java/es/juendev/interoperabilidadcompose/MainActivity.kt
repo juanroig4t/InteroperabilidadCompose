43cf4049cda07a07257c658df6cb0bbac280f542
juanroig4t <93948627+juanroig4t@users.noreply.github.com>
2024-02-17 13:13:02 +0100

    Añadir compose en vista clasica

--- app/src/main/java/es/juendev/interoperabilidadcompose/MainActivity.kt
+++ app/src/main/java/es/juendev/interoperabilidadcompose/MainActivity.kt
@@ -2,10 +2,32 @@ package es.juendev.interoperabilidadcompose
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val composeView = findViewById<ComposeView>(R.id.compose_view)
+
+        composeView.setContent {
+            Column (
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Text(text = "Compose!!!!!!",
+                    style= MaterialTheme.typography.headlineLarge,
+                    color = MaterialTheme.colorScheme.primary
+                )
+            }
+
+        }
     }
 }
